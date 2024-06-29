027ad1aa595d7f01cab6b9de74a88cd0b66bc47c
Scott Campbell <scott@josephscottcampbell.com>
2024-06-29 09:48:07 -0400

    simplified nvim config to reduce risk of malicious plugins

--- nvim/lua/init.lua
+++
@@ -1,7 +0,0 @@
-require "paq" {
-
-    "savq/paq-nvim";                  -- Let Paq manage itself
-    "CRAG666/code_runner.nvim";
-    "nvim-lua/plenary.nvim";
-    use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}}}
-}
