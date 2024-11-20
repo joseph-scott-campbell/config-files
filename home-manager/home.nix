d81810c4551b87aec50f4a4594863281281517c0
Scott Campbell <scott@josephscottcampbell.com>
2024-11-20 06:36:12 -0500

    added home manager

---
+++ home-manager/home.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "user";
+  home.homeDirectory = "/home/user";
+
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  nixpkgs.config.allowUnfree = true; # allowing proprietary software
+  home.packages = [
+    # sanity check program
+    (pkgs.writeShellScriptBin "is-home-manager-working" ''
+        echo "yes it is"
+    '')
+
+    # development
+    # NOTE: very basic stuff like git and vim are in system-wide config
+    pkgs.alacritty
+    pkgs.tmux
+
+    # general productivity
+    pkgs.chromium
+
+    # chat applications
+    pkgs.discord
+    pkgs.signal-desktop
+    
+    # misc
+    pkgs.nerdfonts
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+     EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
