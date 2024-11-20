75d4e510c1265733a15b7f32c0f23b0476e60444
Scott Campbell <scott@josephscottcampbell.com>
2024-11-20 15:13:03 -0500

    created vim plug setup

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -40,13 +40,24 @@
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+ # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    
+    ".config/alacritty.toml".source = ../../alacritty.toml;
+    ".config/nvim/init.vim".source = ../../nvim/init.vim;
+
+    # setup vim plug for neovim
+    # user will still have to <Prefix> + I on first load
+    ".local/share/nvim/site/autoload/plug.vim".text = "";
+    ".local/share/nvim/site/autoload/plug.vim".source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+        sha256 = "1nywzjd9nfr7sqqbdi69wza305q3vp26i0390j1884wdz6awid10";
+    };
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
@@ -55,6 +66,8 @@
     # '';
   };
 
+  # setup various plugin managers
+  # vim plug for neovim
   home.sessionVariables = {
      EDITOR = "nvim";
   };
